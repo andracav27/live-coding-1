06c7446943ec5a404a49cd2788c30b2821649037
andracav27 <andreali.andracava@gmail.com>
2023-07-06 11:45:34 -0600

    Live coding

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// Escribe tu código aquí.
+function bigWords(string, stringArray) {
+    const result = [];
+  
+    for (let i = 0; i < stringArray.length; i++) {
+      if (stringArray[i].length > string.length) {
+        result.push(stringArray[i]);
+      }
+    }
+  
+    return result;
+  }
+  
+ function printArray(stringArray) {
+    const ul = document.getElementById('wordList');
+  
+    for (let i = 0; i < stringArray.length; i++) {
+      const li = document.createElement('li');
+      li.textContent = stringArray[i];
+      ul.appendChild(li);
+    }
+  }
+  
+  const myArray = ['Sopes', 'Tacos', 'Quesadillas', 'Gorditas', 'Tostadas', 'Enchiladas'];
+  const inputString = 'Comida';
+  
+  const resultArray = bigWords(inputString, myArray);
+  printArray(resultArray);
